58c756fae58e401172db407e20dda688ce7e1f24
Gauthier DESCAMPS <gauthier1.descamps@etudiant.univ-lille1.fr>
2018-03-22 09:32:06 +0100

    commit

--- server/src/main/webapp/js/lib.js
+++ server/src/main/webapp/js/lib.js
@@ -1,17 +1,43 @@
-'use strict';
+"use strict";
 
 //SCRIPT QUI GERE LA CONNEXION UTILISATEUR ET LE RETOUR DE CONNEXION
-var mama = 42;
-var mavariable = 1337;
-console.log(mavariable);
+
+var aze = 45;
+var azer = 88;
 
 $("#formConnection").submit(function (event) {
   // des qu'il y a un event appeler la ligne suivante
   event.preventDefault();
 
-  //alert( "Handler for .submit() called." );
-  var text = $('#formConnection').find('input[name="login"]').val();
-  console.log(text);
-  var mdp = $('#formConnection').find('input[name="password"]').val();
-  console.log(mdp);
+  $("#messageErreur").hide();
+  var login = $('#formConnection').find('input[name="login"]').val();
+  var password = $('#formConnection').find('input[name="password"]').val();
+
+  //console.log(login);
+  //console.log(password);
+
+  if (login.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+
+    console.log(JSON.stringify($("form").serializeArray()));
+    $.ajax({
+      url: "",
+      type: "POST",
+      dataType: "json",
+      data: JSON.stringify($("form").serializeArray()),
+      success: function success(json) {},
+      error: function error(xhr, status, errorThrown) {
+        alert("Sorry, there was a problem!");
+        console.log("Error: " + errorThrown);
+        console.log("Status: " + status);
+        console.dir(xhr);
+      },
+      complete: function complete(xhr, status) {
+        console.log('ajax request completed !');
+      }
+    });
+  } else {
+    //Gestion erreur saisie MAIL
+
+    $("#messageErreur").show();
+  }
 });
