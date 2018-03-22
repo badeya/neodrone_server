39d0951d616d1ff461a7f166cb3fd4b9be7de9fb
Simon DEMECE <demeces@epicea09.iut-infobio.priv.univ-lille1.fr>
2018-03-22 17:21:12 +0100

    connexion et début de gestion ajout user

--- server/src/main/webapp/js/lib.js
+++ server/src/main/webapp/js/lib.js
@@ -1,7 +1,5 @@
 "use strict";
 
-var aze = 55;
-
 $("#formConnection").submit(function (event) {
   // des qu'il y a un event appeler la ligne suivante
   event.preventDefault();
@@ -9,14 +7,6 @@ $("#formConnection").submit(function (event) {
   $("#messageErreur").hide();
 
   getWithAuthorizationHeader("/test/login", afficheUser);
-});
-
-function authent() {
-  //console.log("authen");
-}
-
-$("#formAjoutUser").submit(function (event) {
-  event.preventDefault();
 });
 
 function getWithAuthorizationHeader(url, callback) {
@@ -41,10 +31,42 @@ function getWithAuthorizationHeader(url, callback) {
 }
 
 function afficheUser(data) {
-  console.log(data);
-  $("#output").html(userStringify(data));
+  if (data.id > -1) {
+    console.log(data);
+    $("#output").html(makeProfil(data));
+  } else {
+    $("#messageErreur").show();
+  }
 }
 
-function userStringify(user) {
-  return user.id + ". " + user.nom + " &lt;" + user.email + "&gt;" + " (" + user.prenom + ")";
+function makeProfil(user) {
+  var html = "<h3>Bienvenue " + " dans votre espace perso :</h1>";
+  //  let html = "<h3>Bienvenue " +  user.prenom + " " + user.nom+ " dans votre espace perso :</h1>";
+  return html;
+  //return user.id + ". " + user.nom + " &lt;" + user.email + "&gt;" + " (" + user.prenom + ")";
+}
+
+$("#formAjoutUser").submit(function (event) {
+  event.preventDefault();
+  if ($("#ajoutLogin").val() != "" && $("#ajoutPass").val() != "") {
+    $.ajax({
+      type: "POST",
+      url: "/test/utilisateur/insertion",
+      dataType: 'json',
+      beforeSend: function beforeSend(req) {
+        req.setRequestHeader("Authorization", "Basic " + btoa($("#ajoutLogin").val() + ":" + $("#ajoutPassmdp").val() + ":" + $("#ajoutNom").val() + ":" + $("#ajoutPrenom").val() + ":" + $("#ajoutCP").val() + ":" + $("#ajoutTelFixe").val() + ":" + $("#ajoutTelPort").val() + ":" + $("#ajoutFonct").val() + ":" + $("#ajoutEnt").val() + ":" + $("#ajoutRue").val() + ":" + $("#ajoutRue").val() + ":" + $("#ajoutRole").val()));
+      },
+      success: ajoutUtilisateur(),
+      error: function error(jqXHR, textStatus, errorThrown) {
+        alert('error: ' + textStatus);
+      }
+    });
+  }
+
+  //console.log($("#ajoutRole").val());
+});
+
+function ajoutUtilisateur() {
+  //TODO afficher un message apres l'ajout d'une personne en base
+  console.log("ajout utilisateur fait");
 }
