00392e08e3dc44d529105117f18726227aa2877b
badeya <arthur62630@hotmail.fr>
2018-03-22 11:35:19 +0100

    dowload ultra simple

--- server/src/main/java/fr/iutinfo/neodrone/api/DowloadDemo.java
+++ server/src/main/java/fr/iutinfo/neodrone/api/DowloadDemo.java
@@ -7,21 +7,22 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.MediaType;
 
-@Path("/image")
+@Path("/")
 public class DowloadDemo {
 
-	private static final String FILE_PATH = "/home/infoetu/badeya/Bureau/index.jpeg";
+	private static final String FILE_PATH = "/home/infoetu/badeya/Bureau/test.jpeg";
 
 	@GET
-	@Path("/get")
-	@Produces("image/png")
+	@Path("/download")
+	@Produces(MediaType.APPLICATION_OCTET_STREAM)
 	public Response getFile() {
 
 		File file = new File(FILE_PATH);
 
 		ResponseBuilder response = Response.ok((Object) file);
-		response.header("Content-Disposition", "attachment; filename=image_from_server.png");
+		response.header("Content-Disposition", "attachment; filename="+file.getName());
 		return response.build();
 
 	}
