0e65675e9d9aba4d6233bc848c1835d64ccaf8ec
Simon DEMECE <demeces@epicea09.iut-infobio.priv.univ-lille1.fr>
2018-03-23 11:02:10 +0100

    ajout index et lib.js

--- server/src/main/webapp/js/lib.js
+++ server/src/main/webapp/js/lib.js
@@ -98,15 +98,55 @@ function postUser(url, nom, telport, telfixe, entreprise, fonction, rue, prenom,
   });
 }
 
+function updateUser(url, nom, telport, telfixe, entreprise, fonction, rue, prenom, codepostal, ville, email, pwd) {
+  console.log("postUser " + url);
+  $.ajax({
+    type: 'PUT',
+    contentType: 'application/json',
+    url: url,
+    dataType: "json",
+    data: JSON.stringify({
+      "email": email,
+      "password": pwd,
+      "nom": nom,
+      "prenom": prenom,
+      "ville": ville,
+      "codep": codepostal,
+      "rue": rue,
+      "fonction": fonction,
+      "societe": entreprise,
+      "mobile": telfixe,
+      "fixe": telport
+
+    }),
+    success: function success(data, textStatus, jqXHR) {
+      makeUpdateMessage(data);
+    },
+    error: function error(jqXHR, textStatus, errorThrown) {
+      console.log('postUser error: ' + textStatus);
+    }
+  });
+}
+
 function makeProfil(user) {
   var html = "<h3>Bienvenue " + " dans votre espace perso :</h1>";
-  //  let html = "<h3>Bienvenue " +  user.prenom + " " + user.nom+ " dans votre espace perso :</h1>";
+  //Ajout du tableau d'en tête (nom, prenom, logo neodrone, société, fonction)
+  html += "<table border=\"0\" > <tr> <td> <h3>" + user.nom + "</h3></td> <td rowspan=\"2\"><a href=\"http://neodrone.fr\"><img src=\"res/logo.png\" alt=\"Logo neodrone\"></a></td> <td><h3>" + user.societe + "</h3></td> </tr><tr><td><h3>" + user.prenom + "</h3></td> <td><h3>" + user.fonction + "</h3></td></tr></table>";
+  html += "<br>";
+  //Ajout du menu "à onglet"
+  html += "<ul class=\"nav nav-tabs\"> <li class=\"active\"><a data-toggle=\"tab\" href=\"#home\">Home</a></li><li><a data-toggle=\"tab\" href=\"#infos\">INFOS</a></li><li><a data-toggle=\"tab\" href=\"#files\">FILES</a></li></ul>";
+  html += "<div class=\"tab-content\"><div id=\"home\" class=\"tab-pane fade in active\"><h3>HOME</h3><p>BLABLABLA DU HOME</p></div><div id=\"infos\" class=\"tab-pane\"><h3>INFOS</h3><p>BLABLA DES INFOS</p></div><div id=\"files\" class=\"tab-pane fade\"><h3>FILES</h3><div class=\"container\"><div class=\"row\"><div class=\"col-sm-4\"><div id=\"treeview\" class=\"\"></div></div></div></div><script>var defaultData = [{text: \'mission1\',href: \'#parent1\',nodes: [{text: \'FILE 1\',href: \'#child1\', },{text: \'FILE 2\',href: \'#child2\',}]},{text: \'mission2\',href: \'#parent2\',nodes: [{text: \'FILE 1\',icon: \'glyphicon glyphicon-camera\',href: \'localhost:8080\',selectable: true,state: {checked: false,disabled: false,expanded: false,selected: false},},{text: \'FILE 2\',icon: \'glyphicon glyphicon-film\',href: \'http://www.google.fr\',selectable: true,state: {checked: false,disabled: false,expanded: false,selected: false},}]},{text: \'mission3\',href: \'#parent3\',},{text: \'mission4\',href: \'#parent4\',},{text: \'mission5\',href: \'#parent5\' ,}]; $('#treeview').treeview({ color: \"#000000\", data: defaultData,enableLinks: true});</script></div></div>";
+
   return html;
-  //return user.id + ". " + user.nom + " &lt;" + user.email + "&gt;" + " (" + user.prenom + ")";
 }
 
 function makeAjoutMessage(data) {
   var txt2 = $("<p id='newP'></p>").text("Ajout client réussi");
+  $("#output").append(txt2);
+}
+
+function makeUpdateMessage(data) {
+  var txt2 = $("<p id='newP'></p>").text("update client réussi");
   $("#output").append(txt2);
 }
 
@@ -119,14 +159,18 @@ $("#formAjoutUser").submit(function (event) {
   //console.log($("#ajoutRole").val());
 });
 
+$("#formUpdateUser").submit(function (event) {
+  event.preventDefault();
+  updateUser("/test/utilisateur", $("#updateNom").val(), $("#updateTelPort").val(), $("#updateTelFixe").val(), $("#updateEnt").val(), $("#updateFonct").val(), $("#updateRue").val(), $("#updatePrenom").val(), $("#updateCP").val(), $("#updateVille").val(), $("#updateLogin").val(), $("#updatePass").val());
+});
+
 $("#formAjoutMission").submit(function (event) {
   event.preventDefault();
   if ($("#missionNom").val() != "") {
     postMission("/test/mission/", $("#missionClient").val(), $("#missionNom").val(), $("#missionDesc").val(), $("#missionEtat").val());
   }
-
-  //console.log($("#ajoutRole").val());
 });
+
 function ajoutUtilisateur() {
   //TODO afficher un message apres l'ajout d'une personne en base
   console.log("ajout utilisateur fait");
