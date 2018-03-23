b7496af28fdc29ce2910708831abcc07ed1ad63f
Simon DEMECE <demeces@epicea09.iut-infobio.priv.univ-lille1.fr>
2018-03-23 09:59:14 +0100

    modif lib js avec form ajout client et mission

--- server/src/main/webapp/js/lib.js
+++ server/src/main/webapp/js/lib.js
@@ -39,6 +39,65 @@ function afficheUser(data) {
   }
 }
 
+function makeAjoutMissionMessage() {
+  var txt2 = $("<p id='newP'></p>").text("Ajout Mission Réussi");
+  $("#output").append(txt2);
+}
+
+function postMission(url, email, mission, description, etat) {
+  console.log("postUser " + url);
+  $.ajax({
+    type: 'POST',
+    contentType: 'application/json',
+    url: url,
+    dataType: "json",
+    data: JSON.stringify({
+      "client": email,
+      "mission": mission,
+      "description": description,
+      "etat": etat
+
+    }),
+    success: function success(data, textStatus, jqXHR) {
+      makeAjoutMissionMessage(data);
+    },
+    error: function error(jqXHR, textStatus, errorThrown) {
+      console.log('postUser error: ' + textStatus);
+    }
+  });
+}
+
+function postUser(url, nom, telport, telfixe, entreprise, fonction, rue, prenom, codepostal, ville, email, pwd, role) {
+  console.log("postUser " + url);
+  $.ajax({
+    type: 'POST',
+    contentType: 'application/json',
+    url: url,
+    dataType: "json",
+    data: JSON.stringify({
+      "email": email,
+      "password": pwd,
+      "role": role,
+      "nom": nom,
+      "prenom": prenom,
+      "ville": ville,
+      "codep": codepostal,
+      "rue": rue,
+      "fonction": fonction,
+      "societe": entreprise,
+      "mobile": telfixe,
+      "fixe": telport
+
+    }),
+    success: function success(data, textStatus, jqXHR) {
+      makeAjoutMessage(data);
+    },
+    error: function error(jqXHR, textStatus, errorThrown) {
+      console.log('postUser error: ' + textStatus);
+    }
+  });
+}
+
 function makeProfil(user) {
   var html = "<h3>Bienvenue " + " dans votre espace perso :</h1>";
   //  let html = "<h3>Bienvenue " +  user.prenom + " " + user.nom+ " dans votre espace perso :</h1>";
@@ -46,26 +105,28 @@ function makeProfil(user) {
   //return user.id + ". " + user.nom + " &lt;" + user.email + "&gt;" + " (" + user.prenom + ")";
 }
 
+function makeAjoutMessage(data) {
+  var txt2 = $("<p id='newP'></p>").text("Ajout client réussi");
+  $("#output").append(txt2);
+}
+
 $("#formAjoutUser").submit(function (event) {
   event.preventDefault();
   if ($("#ajoutLogin").val() != "" && $("#ajoutPass").val() != "") {
-    $.ajax({
-      type: "POST",
-      url: "/test/utilisateur/insertion",
-      dataType: 'json',
-      beforeSend: function beforeSend(req) {
-        req.setRequestHeader("Authorization", "Basic " + btoa($("#ajoutLogin").val() + ":" + $("#ajoutPassmdp").val() + ":" + $("#ajoutNom").val() + ":" + $("#ajoutPrenom").val() + ":" + $("#ajoutCP").val() + ":" + $("#ajoutTelFixe").val() + ":" + $("#ajoutTelPort").val() + ":" + $("#ajoutFonct").val() + ":" + $("#ajoutEnt").val() + ":" + $("#ajoutRue").val() + ":" + $("#ajoutRue").val() + ":" + $("#ajoutRole").val()));
-      },
-      success: ajoutUtilisateur(),
-      error: function error(jqXHR, textStatus, errorThrown) {
-        alert('error: ' + textStatus);
-      }
-    });
+    postUser("/test/utilisateur/", $("#ajoutNom").val(), $("#ajoutTelPort").val(), $("#ajoutTelFixe").val(), $("#ajoutEnt").val(), $("#ajoutFonct").val(), $("#ajoutRue").val(), $("#ajoutPrenom").val(), $("#ajoutCP").val(), $("#ajoutVille").val(), $("#ajoutLogin").val(), $("#ajoutPass").val(), $("#ajoutRole").val());
   }
 
   //console.log($("#ajoutRole").val());
 });
 
+$("#formAjoutMission").submit(function (event) {
+  event.preventDefault();
+  if ($("#missionNom").val() != "") {
+    postMission("/test/mission/", $("#missionClient").val(), $("#missionNom").val(), $("#missionDesc").val(), $("#missionEtat").val());
+  }
+
+  //console.log($("#ajoutRole").val());
+});
 function ajoutUtilisateur() {
   //TODO afficher un message apres l'ajout d'une personne en base
   console.log("ajout utilisateur fait");
