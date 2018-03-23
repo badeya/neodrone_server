2fd74d4e4a3bc88d57d2a502861bfbe79915a80f
Simon DEMECE <demeces@epicea09.iut-infobio.priv.univ-lille1.fr>
2018-03-23 16:53:18 +0100

    commit integration client

--- server/src/main/webapp/js/lib.js
+++ server/src/main/webapp/js/lib.js
@@ -1,266 +1,211 @@
 "use strict";
 
 $("#formConnection").submit(function (event) {
-  // des qu'il y a un event appeler la ligne suivante
-  event.preventDefault();
+    // des qu'il y a un event appeler la ligne suivante
+    event.preventDefault();
 
-  $("#messageErreur").hide();
+    $("#messageErreur").hide();
 
-  getWithAuthorizationHeader("/test/login", afficheUser);
+    getWithAuthorizationHeader("/test/login", afficheUser);
 });
 
 function getWithAuthorizationHeader(url, callback) {
-  if ($("#login").val() != "") {
-    $.ajax({
-      type: "GET",
-      url: url,
-      dataType: 'json',
-      beforeSend: function beforeSend(req) {
-        req.setRequestHeader("Authorization", "Basic " + btoa($("#login").val() + ":" + $("#mdp").val()));
-      },
-      success: callback,
-      error: function error(jqXHR, textStatus, errorThrown) {
-        alert('error: ' + textStatus);
-      }
-    });
-  } else {
-    $.getJSON(url, function (data) {
-      afficheUser(data);
-    });
-  }
+    if ($("#login").val() != "") {
+        $.ajax({
+            type: "GET",
+            url: url,
+            dataType: 'json',
+            beforeSend: function beforeSend(req) {
+                req.setRequestHeader("Authorization", "Basic " + btoa($("#login").val() + ":" + $("#mdp").val()));
+            },
+            success: callback,
+            error: function error(jqXHR, textStatus, errorThrown) {
+                alert('error: ' + textStatus);
+            }
+        });
+    } else {
+        $.getJSON(url, function (data) {
+            afficheUser(data);
+        });
+    }
 }
 
 function afficheUser(data) {
-  if (data.id > -1) {
-    console.log(data);
-    if (data.role == "client") $("#output").html(makeProfil(data));else if (data.role == "admin") $("#output").html(makeAdminProfil(data));
-  } else {
-    $("#messageErreur").show();
-  }
+    if (data.id > -1) {
+        console.log(data);
+        if (data.role == "client") {
+            $("#output").html(makeProfil(data));
+            initFiles();
+        } else if (data.role == "admin") {
+            $("#output").html(makeAdminProfil(data));
+            initFiles();
+        }
+    } else {
+        $("#messageErreur").show();
+    }
 }
 
 function makeAjoutMissionMessage() {
-  var txt2 = $("<p id='newP'></p>").text("Ajout Mission Réussi");
-  $("#output").append(txt2);
+    var txt2 = $("<p id='newP'></p>").text("Ajout Mission Réussi");
+    $("#output").append(txt2);
 }
 
 function postMission(url, email, mission, description, etat) {
-  console.log("postUser " + url);
-  $.ajax({
-    type: 'POST',
-    contentType: 'application/json',
-    url: url,
-    dataType: "json",
-    data: JSON.stringify({
-      "client": email,
-      "mission": mission,
-      "description": description,
-      "etat": etat
+    console.log("postUser " + url);
+    $.ajax({
+        type: 'POST',
+        contentType: 'application/json',
+        url: url,
+        dataType: "json",
+        data: JSON.stringify({
+            "client": email,
+            "mission": mission,
+            "description": description,
+            "etat": etat
 
-    }),
-    success: function success(data, textStatus, jqXHR) {
-      makeAjoutMissionMessage(data);
-    },
-    error: function error(jqXHR, textStatus, errorThrown) {
-      console.log('postUser error: ' + textStatus);
-    }
-  });
+        }),
+        success: function success(data, textStatus, jqXHR) {
+            makeAjoutMissionMessage(data);
+        },
+        error: function error(jqXHR, textStatus, errorThrown) {
+            console.log('postUser error: ' + textStatus);
+        }
+    });
 }
 
 function postUser(url, nom, telport, telfixe, entreprise, fonction, rue, prenom, codepostal, ville, email, pwd, role) {
-  console.log("postUser " + url);
-  $.ajax({
-    type: 'POST',
-    contentType: 'application/json',
-    url: url,
-    dataType: "json",
-    data: JSON.stringify({
-      "email": email,
-      "password": pwd,
-      "role": role,
-      "nom": nom,
-      "prenom": prenom,
-      "ville": ville,
-      "codep": codepostal,
-      "rue": rue,
-      "fonction": fonction,
-      "societe": entreprise,
-      "mobile": telfixe,
-      "fixe": telport
+    console.log("postUser " + url);
+    $.ajax({
+        type: 'POST',
+        contentType: 'application/json',
+        url: url,
+        dataType: "json",
+        data: JSON.stringify({
+            "email": email,
+            "password": pwd,
+            "role": role,
+            "nom": nom,
+            "prenom": prenom,
+            "ville": ville,
+            "codep": codepostal,
+            "rue": rue,
+            "fonction": fonction,
+            "societe": entreprise,
+            "mobile": telfixe,
+            "fixe": telport
 
-    }),
-    success: function success(data, textStatus, jqXHR) {
-      makeAjoutMessage(data);
-    },
-    error: function error(jqXHR, textStatus, errorThrown) {
-      console.log('postUser error: ' + textStatus);
-    }
-  });
+        }),
+        success: function success(data, textStatus, jqXHR) {
+            makeAjoutMessage(data);
+        },
+        error: function error(jqXHR, textStatus, errorThrown) {
+            console.log('postUser error: ' + textStatus);
+        }
+    });
 }
 
 function updateUser(url, nom, telport, telfixe, entreprise, fonction, rue, prenom, codepostal, ville, email, pwd) {
-  console.log("postUser " + url);
-  $.ajax({
-    type: 'PUT',
-    contentType: 'application/json',
-    url: url,
-    dataType: "json",
-    data: JSON.stringify({
-      "email": email,
-      "password": pwd,
-      "nom": nom,
-      "prenom": prenom,
-      "ville": ville,
-      "codep": codepostal,
-      "rue": rue,
-      "fonction": fonction,
-      "societe": entreprise,
-      "mobile": telfixe,
-      "fixe": telport
+    console.log("postUser " + url);
+    $.ajax({
+        type: 'PUT',
+        contentType: 'application/json',
+        url: url,
+        dataType: "json",
+        data: JSON.stringify({
+            "email": email,
+            "password": pwd,
+            "nom": nom,
+            "prenom": prenom,
+            "ville": ville,
+            "codep": codepostal,
+            "rue": rue,
+            "fonction": fonction,
+            "societe": entreprise,
+            "mobile": telfixe,
+            "fixe": telport
 
-    }),
-    success: function success(data, textStatus, jqXHR) {
-      makeUpdateMessage(data);
-    },
-    error: function error(jqXHR, textStatus, errorThrown) {
-      console.log('postUser error: ' + textStatus);
-    }
-  });
+        }),
+        success: function success(data, textStatus, jqXHR) {
+            makeUpdateMessage(data);
+        },
+        error: function error(jqXHR, textStatus, errorThrown) {
+            console.log('postUser error: ' + textStatus);
+        }
+    });
 }
 
 function makeAdminProfil(user) {
-  var html = "<h3>Bienvenue " + " dans votre espace admin :</h1>";
-  return html;
+    var html = "<h3>Bienvenue dans votre espace admin :</h1>";
+    return html;
 }
 
 function makeProfil(user) {
-  var html = "<h3>Bienvenue dans votre espace perso :</h1>";
-  //Ajout du tableau d'en tête (nom, prenom, logo neodrone, société, fonction)
-  html += "<table border=\"0\" > <tr> <td> <h3>" + user.nom + "</h3></td> <td rowspan=\"2\"><a href=\"http://neodrone.fr\"><img src=\"res/logo.png\" alt=\"Logo neodrone\"></a></td> <td><h3>" + user.societe + "</h3></td> </tr><tr><td><h3>" + user.prenom + "</h3></td> <td><h3>" + user.fonction + "</h3></td></tr></table>";
-  html += "<br>";
-  //Ajout du menu "à onglet";
-  html += "<ul class=\"nav nav-tabs\">";
-  html += "<li class=\"active\">";
-  html += "<a data-toggle=\"tab\" href=\"#home\">Home</a></li>";
-  html += "<li><a data-toggle=\"tab\" href=\"#infos\">INFOS</a></li>";
-  html += "<li><a data-toggle=\"tab\" href=\"#files\">FILES</a></li></ul>";
-  html += "<div class=\"tab-content\"><div id=\"home\" class=\"tab-pane fade in active\">";
-  html += "<h3>HOME</h3>";
-  /**************************/
+    var html = "\n  <h3>Bienvenue dans votre espace perso :</h1>\n  <!--Ajout du tableau d'en t\xEAte (nom, prenom, logo neodrone, soci\xE9t\xE9, fonction)-->\n  <table border=\"0\" > \n      <tr> \n          <td> \n              <h3>" + user.nom + "</h3>\n          </td> \n          <td rowspan=\"2\">\n              <a href=\"http://neodrone.fr\"><img src=\"res/logo.png\" alt=\"Logo neodrone\"></a>\n          </td> \n          <td>\n              <h3> " + user.societe + " </h3>\n          </td> \n      </tr>\n      <tr>\n          <td>\n              <h3>" + user.prenom + "</h3>\n          </td> \n          <td>\n              <h3>" + user.fonction + "</h3>\n          </td>\n      </tr>\n  </table>\n  <br>\n <!-->Ajout du menu \"\xE0 onglet-->\n  <ul class=\"nav nav-tabs\">\n      <li class=\"active\">\n          <a data-toggle=\"tab\" href=\"#home\">Mon compte</a>\n      </li>\n      <li>\n          <a data-toggle=\"tab\" href=\"#infos\">Mes missions</a>\n      </li>\n      <li>\n          <a data-toggle=\"tab\" href=\"#files\">Mes fichiers</a>\n      </li>\n  </ul>\n  <div class=\"tab-content\">\n      <div id=\"home\" class=\"tab-pane fade in active\">\n          <h3>HOME</h3>\n          <p>Modif d'un utilisateur :</p>\n          <form id=\"formUpdateUser\" method=\"POST\">\n              <label for=\"updateLogin\">Mail :</label>\n              <input type=\"text\" id=\"updateLogin\">\n              </br>\n              <label for=\"updatePass\">Mot de passe :</label>\"\n              <input type=\"text\" id=\"updatePass\" >\n              </br>\n              <label for=\"updateNom\">Nom :</label>\n              <input type=\"text\" id=\"updateNom\">\n              </br>\n              <label for=\"updatePrenom\">Prenom :</label>\n              <input type=\"text\" id=\"updatePrenom\">\n              </br>\n              <label for=\"updateVille\">Ville :</label>\n              <input type=\"text\" id=\"updateVille\">\n              </br>\n              <label for=\"updateCP\">Code Postal :</label>\n              <input type=\"text\" id=\"updateCP\">\n              </br>\n              <label for=\"updateRue\">Rue :</label>\n              <input type=\"text\" id=\"updateRue\">\n              </br>\n              <label for=\"updateFonct\">Fonction :</label>\n              <input type=\"text\" id=\"updateFonct\">\n              </br>\n              <label for=\"updateEnt\">Entreprise :</label>\n              <input type=\"text\" id=\"updateEnt\">\n              </br>\n              <label for=\"updateTelFixe\">T\xE9l\xE9phone fixe :</label>\n              <input type=\"text\" id=\"updateTelFixe\">\n              </br>\n              <label for=\"updateTelPort\">T\xE9l\xE9phone portable :</label>\n              <input type=\"text\" id=\"updateTelPort\">\n              </br>\n              </br>\n              <input type=\"submit\" id=\"btnUpdateUser\" value=\"Update utilisateur\">\n          </form>\n      </div>\n    \n\n\n\n  <div id=\"infos\" class=\"tab-pane\">\n      <h3>INFOS</h3>\n      <p>BLABLA DES INFOS</p>\n  </div>\n  \n  \n  \n  <div id=\"files\" class=\"tab-pane fade\">                        \n      <h3>FILES</h3>\n      <div class=\"container\">\n          <div class=\"row\">\n              <div class=\"col-sm-4\">\n                  <div id=\"treeview\" class=\"\">\n                  </div>\n              </div>\n          </div>\n      </div>\n  </div>";
+    return html;
+}
 
-  html += "<p>Modif d'un utilisateur :</p>";
-  html += "<form id=\"formUpdateUser\" method=\"POST\">";
-  html += "<label for=\"updateLogin\">Mail :</label>";
-  html += "<input type=\"text\" id=\"updateLogin\">";
-  html += "</br>";
-  html += "<label for=\"updatePass\">Mot de passe :</label>";
-  html += "<input type=\"text\" id=\"updatePass\" >";
-  html += "</br>";
-  html += "<label for=\"updateNom\">Nom :</label>";
-  html += "<input type=\"text\" id=\"updateNom\">";
-  html += "</br>";
-  html += "<label for=\"updatePrenom\">Prenom :</label>";
-  html += "<input type=\"text\" id=\"updatePrenom\">";
-  html += "</br>";
-  html += "<label for=\"updateVille\">Ville :</label>";
-  html += "<input type=\"text\" id=\"updateVille\">";
-  html += "</br>";
-  html += "<label for=\"updateCP\">Code Postal :</label>";
-  html += "<input type=\"text\" id=\"updateCP\">";
-  html += "</br>";
-  html += "<label for=\"updateRue\">Rue :</label>";
-  html += "<input type=\"text\" id=\"updateRue\">";
-  html += "</br>";
-  html += "<label for=\"updateFonct\">Fonction :</label>";
-  html += "<input type=\"text\" id=\"updateFonct\">";
-  html += "</br>";
-  html += "<label for=\"updateEnt\">Entreprise :</label>";
-  html += "<input type=\"text\" id=\"updateEnt\">";
-  html += "</br>";
-  html += "<label for=\"updateTelFixe\"\>Téléphone fixe :</label>";
-  html += "<input type=\"text\" id=\"updateTelFixe\">";
-  html += "</br>";
-  html += "<label for=\"updateTelPort\">Téléphone portable :</label>";
-  html += "<input type=\"text\" id=\"updateTelPort\">";
-  html += "</br>";
-  html += "</br>";
-  html += "<input type=\"submit\" id=\"btnUpdateUser\" value=\"Update utilisateur\">";
-  html += "</form>";
+function initFiles() {
+    var defaultData = [{
+        text: 'mission1',
+        href: '#parent1',
+        nodes: [{
+            text: 'FILE 1',
+            href: '#child1' }, { text: 'FILE 2',
+            href: '#child2' }] }, {
+        text: 'mission2',
+        href: '#parent2',
+        nodes: [{
+            text: 'FILE 1',
+            icon: 'glyphicon glyphicon-camera',
+            href: 'localhost:8080',
+            selectable: true
+        }, {
+            text: 'FILE 2',
+            icon: 'glyphicon glyphicon-film',
+            href: 'http://www.google.fr',
+            selectable: true,
+            state: {
+                checked: false,
+                disabled: false,
+                expanded: false,
+                selected: false
+            }
+        }]
+    }, { text: 'mission3',
+        href: '#parent3' }, { text: 'mission4',
+        href: '#parent4' }, { text: 'mission5',
+        href: '#parent5' }];
 
-  /********************/
-
-  html += "<div id=\"infos\" class=\"tab-pane\">";
-  html += "<h3>INFOS</h3><p>BLABLA DES INFOS</p></div>";
-  html += "<div id=\"files\" class=\"tab-pane fade\">";
-  html += "<h3>FILES</h3>";
-  html += "<div class=\"container\">";
-  html += "<div class=\"row\"><div class=\"col-sm-4\"><div id=\"treeview\" class=\"\"></div></div></div></div>";
-  html += "<script>";
-  html += "var defaultData = [{";
-  html += "text: \'mission1\',";
-  html += "href: \'#parent1\',";
-  html += "nodes: [{";
-  html += "text: \'FILE 1\',";
-  html += "href: \'#child1\', },";
-  html += "{text: \'FILE 2\',";
-  html += "href: \'#child2\',}]},";
-  html += "{text: \'mission2\',";
-  html += "href: \'#parent2\',";
-  html += "nodes: [{text: \'FILE 1\',";
-  html += "icon: \'glyphicon glyphicon-camera\',";
-  html += "href: \'localhost:8080\',";
-  html += "selectable: true,";
-  html += "state: {";
-  html += "checked: false,";
-  html += "disabled: false,";
-  html += "expanded: false,";
-  html += "selected: false},},";
-  html += "{text: \'FILE 2\',";
-  html += "icon: \'glyphicon glyphicon-film\',";
-  html += "href: \'http://www.google.fr\',";
-  html += "selectable: true,";
-  html += "state: {";
-  html += "checked: false,";
-  html += "disabled: false,";
-  html += "expanded: false,";
-  html += "selected: false},}]},";
-  html += "{text: \'mission3\',";
-  html += "href: \'#parent3\',},";
-  html += "{text: \'mission4\',";
-  html += "href: \'#parent4\',},";
-  html += "{text: \'mission5\',";
-  html += "href: \'#parent5\' ,}];";
-  html += "$('#treeview').treeview({";
-  html += " color: \"#000000\",";
-  html += " data: defaultData,enableLinks: true});</script></div></div>";
-  return html;
+    $('#treeview').treeview({
+        color: "#000000",
+        data: defaultData, enableLinks: true });
 }
 
 function makeAjoutMessage(data) {
-  var txt2 = $("<p id='newP'></p>").text("Ajout client réussi");
-  $("#output").append(txt2);
+    var txt2 = $("<p id='newP'></p>").text("Ajout client réussi");
+    $("#output").append(txt2);
 }
 
 function makeUpdateMessage(data) {
-  var txt2 = $("<p id='newP'></p>").text("update client réussi");
-  $("#output").append(txt2);
+    var txt2 = $("<p id='newP'></p>").text("update client réussi");
+    $("#output").append(txt2);
 }
 
 $("#formAjoutUser").submit(function (event) {
-  event.preventDefault();
-  if ($("#ajoutLogin").val() != "" && $("#ajoutPass").val() != "") {
-    postUser("/test/utilisateur/", $("#ajoutNom").val(), $("#ajoutTelPort").val(), $("#ajoutTelFixe").val(), $("#ajoutEnt").val(), $("#ajoutFonct").val(), $("#ajoutRue").val(), $("#ajoutPrenom").val(), $("#ajoutCP").val(), $("#ajoutVille").val(), $("#ajoutLogin").val(), $("#ajoutPass").val(), $("#ajoutRole").val());
-  }
+    event.preventDefault();
+    if ($("#ajoutLogin").val() != "" && $("#ajoutPass").val() != "") {
+        postUser("/test/utilisateur/", $("#ajoutNom").val(), $("#ajoutTelPort").val(), $("#ajoutTelFixe").val(), $("#ajoutEnt").val(), $("#ajoutFonct").val(), $("#ajoutRue").val(), $("#ajoutPrenom").val(), $("#ajoutCP").val(), $("#ajoutVille").val(), $("#ajoutLogin").val(), $("#ajoutPass").val(), $("#ajoutRole").val());
+    }
 });
 
 $("#formUpdateUser").submit(function (event) {
-  event.preventDefault();
-  updateUser("/test/utilisateur/", $("#updateNom").val(), $("#updateTelPort").val(), $("#updateTelFixe").val(), $("#updateEnt").val(), $("#updateFonct").val(), $("#updateRue").val(), $("#updatePrenom").val(), $("#updateCP").val(), $("#updateVille").val(), $("#updateLogin").val(), $("#updatePass").val());
+    event.preventDefault();
+    updateUser("/test/utilisateur/", $("#updateNom").val(), $("#updateTelPort").val(), $("#updateTelFixe").val(), $("#updateEnt").val(), $("#updateFonct").val(), $("#updateRue").val(), $("#updatePrenom").val(), $("#updateCP").val(), $("#updateVille").val(), $("#updateLogin").val(), $("#updatePass").val());
 });
 
 $("#formAjoutMission").submit(function (event) {
-  event.preventDefault();
-  if ($("#missionNom").val() != "") {
-    postMission("/test/mission/", $("#missionClient").val(), $("#missionNom").val(), $("#missionDesc").val(), $("#missionEtat").val());
-  }
+    event.preventDefault();
+    if ($("#missionNom").val() != "") {
+        postMission("/test/mission/", $("#missionClient").val(), $("#missionNom").val(), $("#missionDesc").val(), $("#missionEtat").val());
+    }
 });
