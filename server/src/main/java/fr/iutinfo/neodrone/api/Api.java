1ce80f69ffb3390a2640e3eff82d041771016bdd
sengesc <charles.senges@etudiant.univ-lille1.fr>
2018-03-26 10:14:00 +0200

    correction des merges

--- server/src/main/java/fr/iutinfo/neodrone/api/Api.java
+++ server/src/main/java/fr/iutinfo/neodrone/api/Api.java
@@ -1,17 +1,19 @@
 package fr.iutinfo.neodrone.api;
 
-import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
-
 import javax.ws.rs.ApplicationPath;
+
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.glassfish.jersey.server.ResourceConfig;
 
 @ApplicationPath("/test/")
 public class Api extends ResourceConfig {
 
-    public Api() {
-        packages("fr.iutinfo.skeleton.api");
-        //register(LoggingFilter.class);
-        
-    }
+	public Api() {
+		packages("fr.iutinfo.neodrone.api");
+		//register(LoggingFilter.class);
+		// packages("org.glassfish.jersey.media.multipart");
+		register(MultiPartFeature.class);
+		// packages("org.glassfish.jersey.media.multipart.internal");
+	}
 
 }
