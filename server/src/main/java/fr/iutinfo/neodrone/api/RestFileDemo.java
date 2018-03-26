07010efa74ded7a06d4572f7a03fdaf655aa5bd9
sengesc <charles.senges@etudiant.univ-lille1.fr>
2018-03-26 10:50:11 +0200

    few add

--- server/src/main/java/fr/iutinfo/neodrone/api/RestFileDemo.java
+++ server/src/main/java/fr/iutinfo/neodrone/api/RestFileDemo.java
@@ -5,6 +5,9 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.Timestamp;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -22,6 +25,8 @@ import fr.iutinfo.skeleton.auth.AuthFilter;
 
 @Path("/")
 public class RestFileDemo {
+	
+	private String uploadRoot = "/home/infoetu/sengesc/neodrone/neodrone_serveur/server/src/main/webapp/uploads/";
 
 	@POST
 	@Path("/upload")
@@ -30,17 +35,23 @@ public class RestFileDemo {
 			@FormDataParam("exampleFileUpload") InputStream file,
 			@FormDataParam("exampleFileUpload") FormDataContentDisposition fileDetail) {
 		
+		// --- LOG
 		Logger logger = LoggerFactory.getLogger(AuthFilter.class);
 		logger.debug("Trying to upload file...");
+		// --- ENDOF LOG
 		
-		String uploadedFileLocation = "/home/infoetu/sengesc/neodrone/neodrone_serveur/server/src/main/webapp/uploads/"
-				+ fileDetail.getFileName();
+		String originalFileName = fileDetail.getFileName();
+		String serverFileName = generateFileName(originalFileName);
+		
+		String uploadedFileLocation = uploadRoot + serverFileName;
 
 		// save it
 		writeToFile(file, uploadedFileLocation);
 
+		// --- LOG
 		String output = "File uploaded to : " + uploadedFileLocation;
 		logger.debug(output);
+		// --- ENDOF LOG
 
 		return Response.status(200).entity(output).build();
 	}
@@ -65,5 +76,22 @@ public class RestFileDemo {
 		}
 
 	}
-
+	
+	private String generateFileName(String original) {
+		String raw = original + (new Timestamp(System.currentTimeMillis()));
+		
+		MessageDigest messageDigest = null;
+		
+		try {
+			messageDigest = MessageDigest.getInstance("SHA-256");
+		} catch (NoSuchAlgorithmException e) {
+			System.exit(1);
+		}
+		
+		messageDigest.update(raw.getBytes());
+		
+		String hashName = new String(messageDigest.digest());
+		
+		return hashName;
+	}
 }
